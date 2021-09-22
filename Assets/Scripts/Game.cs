90f9b59a50f0e8b9c11eb562852d2b8492a38cfc
iDownward <matheus-1199@hotmail.com>
2021-09-22 18:58:06 -0300

    comment google play code

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,103 +7,104 @@ using TMPro;
 
 public class Game : MonoBehaviour
 {
-    public static Text lifeText;
-    public static Text emojiText;
-    public static int life = 30;
-    public static int totalEmojis = 0;
-    public static int maxEmojis = 16;
-    public static int currentSceneIndex = 0;
-    public static bool warped = false;
+  public static Text lifeText;
+  public static Text emojiText;
+  public static int life = 30;
+  public static int totalEmojis = 0;
+  public static int maxEmojis = 16;
+  public static int currentSceneIndex = 0;
+  public static bool warped = false;
 
-    void Start()
+  void Start()
+  {
+    TranslateIfNecessary();
+    // if (currentSceneIndex == 0)
+    // {
+    //   GooglePlayGame.Init();
+    //   GooglePlayGame.Login((success) => { });
+    // }
+    lifeText = GameObject.Find("LifeText").GetComponent<Text>();
+    emojiText = GameObject.Find("EmojiText").GetComponent<Text>();
+    if (PlayerPrefs.HasKey("life"))
     {
-        TranslateIfNecessary();
-        if(currentSceneIndex == 0) {
-            GooglePlayGame.Init();
-            GooglePlayGame.Login((success) => {});
-        }
-        lifeText = GameObject.Find("LifeText").GetComponent<Text>();
-        emojiText = GameObject.Find("EmojiText").GetComponent<Text>();
-        if (PlayerPrefs.HasKey("life"))
-        {
-            life = PlayerPrefs.GetInt("life");
-        }
-        if (PlayerPrefs.HasKey("totalEmojis"))
-        {
-            totalEmojis = PlayerPrefs.GetInt("totalEmojis");
-        }
-        lifeText.text = "x" + life;
-        emojiText.text = "x" + totalEmojis + "/" + maxEmojis;
-        if (totalEmojis >= maxEmojis)
-        {
-            GameObject.Find("ChosenText").GetComponent<TextMeshPro>().text = "game over";
-        }
+      life = PlayerPrefs.GetInt("life");
     }
+    if (PlayerPrefs.HasKey("totalEmojis"))
+    {
+      totalEmojis = PlayerPrefs.GetInt("totalEmojis");
+    }
+    lifeText.text = "x" + life;
+    emojiText.text = "x" + totalEmojis + "/" + maxEmojis;
+    if (totalEmojis >= maxEmojis)
+    {
+      GameObject.Find("ChosenText").GetComponent<TextMeshPro>().text = "game over";
+    }
+  }
 
-    public static void ChangeScene()
+  public static void ChangeScene()
+  {
+    Scene scene = SceneManager.GetActiveScene();
+    warped = true;
+    if (scene.name == "Top")
     {
-        Scene scene = SceneManager.GetActiveScene();
-        warped = true;
-        if (scene.name == "Top")
-        {
-            currentSceneIndex = 1;
-            SceneManager.LoadScene("Bottom");
-        }
-        if (scene.name == "Bottom")
-        {
-            currentSceneIndex = 0;
-            SceneManager.LoadScene("Top");
-        }
+      currentSceneIndex = 1;
+      SceneManager.LoadScene("Bottom");
     }
+    if (scene.name == "Bottom")
+    {
+      currentSceneIndex = 0;
+      SceneManager.LoadScene("Top");
+    }
+  }
 
-    public static void CollectEmoji(string emojiName)
+  public static void CollectEmoji(string emojiName)
+  {
+    totalEmojis += 1;
+    emojiText.text = "x" + totalEmojis + "/" + maxEmojis;
+    PlayerPrefs.SetInt("totalEmojis", totalEmojis);
+    PlayerPrefs.SetInt(emojiName, 1);
+    if (totalEmojis == maxEmojis)
     {
-        totalEmojis += 1;
-        emojiText.text = "x" + totalEmojis + "/" + maxEmojis;
-        PlayerPrefs.SetInt("totalEmojis", totalEmojis);
-        PlayerPrefs.SetInt(emojiName, 1);
-        if (totalEmojis == maxEmojis)
-        {
-            GooglePlayGame.ReportAchievementProgress("CgkItbyBp-sLEAIQAA", 100.0f, (success) => {});
-            Destroy(GameObject.Find("Warp"));
-            Destroy(GameObject.Find("EnemyDemon"));
-            Destroy(GameObject.Find("EnemyShooter"));
-            Destroy(GameObject.Find("EnemyBike"));
-            Destroy(GameObject.Find("EnemyPlane"));
-            GameObject.Find("ChosenText").GetComponent<TextMeshPro>().text = "GAME OVER";
-        }
+      //GooglePlayGame.ReportAchievementProgress("CgkItbyBp-sLEAIQAA", 100.0f, (success) => { });
+      Destroy(GameObject.Find("Warp"));
+      Destroy(GameObject.Find("EnemyDemon"));
+      Destroy(GameObject.Find("EnemyShooter"));
+      Destroy(GameObject.Find("EnemyBike"));
+      Destroy(GameObject.Find("EnemyPlane"));
+      GameObject.Find("ChosenText").GetComponent<TextMeshPro>().text = "GAME OVER";
     }
+  }
 
-    public static bool EmojiCollected(string emojiName)
-    {
-        return PlayerPrefs.HasKey(emojiName);
-    }
+  public static bool EmojiCollected(string emojiName)
+  {
+    return PlayerPrefs.HasKey(emojiName);
+  }
 
-    public static bool CanPlay()
-    {
-        return life > 0;
-    }
+  public static bool CanPlay()
+  {
+    return life > 0;
+  }
 
-    public static void DecreaseLife()
-    {
-        life -= 1;
-        lifeText.text = "x" + life;
-        PlayerPrefs.SetInt("life", life);
-    }
+  public static void DecreaseLife()
+  {
+    life -= 1;
+    lifeText.text = "x" + life;
+    PlayerPrefs.SetInt("life", life);
+  }
 
-    private void TranslateIfNecessary()
+  private void TranslateIfNecessary()
+  {
+    Scene scene = SceneManager.GetActiveScene();
+    bool isPortuguese = Application.systemLanguage == SystemLanguage.Portuguese;
+    if (isPortuguese)
     {
-        Scene scene = SceneManager.GetActiveScene();
-        bool isPortuguese = Application.systemLanguage == SystemLanguage.Portuguese;
-        if (isPortuguese)
-        {
-            if (scene.name == "Top")
-            {
-                GameObject.Find("ChosenText").GetComponent<TextMeshPro>().text = "evite textos";
-                GameObject.Find("ChosenText2").GetComponent<TextMeshPro>().text = "liberte   emojis";
-            }
-            GameObject.Find("AdText").GetComponent<TextMeshPro>().text = "Assista um video para continuar jogando";
-            GameObject.Find("AdButtonText").GetComponent<TextMeshPro>().text = "assistir";
-        }
+      if (scene.name == "Top")
+      {
+        GameObject.Find("ChosenText").GetComponent<TextMeshPro>().text = "evite textos";
+        GameObject.Find("ChosenText2").GetComponent<TextMeshPro>().text = "liberte   emojis";
+      }
+      GameObject.Find("AdText").GetComponent<TextMeshPro>().text = "Assista um video para continuar jogando";
+      GameObject.Find("AdButtonText").GetComponent<TextMeshPro>().text = "assistir";
     }
+  }
 }
